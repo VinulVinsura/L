a1dc33f4fc7126cb498fd9e28b20ef35df755e6f
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-07 10:17:22 +0530

    Create Book Dto class

---
+++ src/main/java/com/example/libaraymanagementsystem/dto/BookDto.java
@@ -0,0 +1,16 @@
+package com.example.libaraymanagementsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+    private String isbn;
+    private String title;
+    private String category;
+    private Integer price;
+    private Integer qty;
+}
