8fb544b1c3a29035188cd3a1622906ffea62dd77
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-07 09:55:46 +0530

    first commit

---
+++ src/main/java/com/example/libaraymanagementsystem/LibarayManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.libaraymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarayManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibarayManagementSystemApplication.class, args);
+    }
+
+}
